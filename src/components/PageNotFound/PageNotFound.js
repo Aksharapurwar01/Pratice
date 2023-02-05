4a5be45523dad86847e29922562fd1c7f156a72f
Akshara Purwar <akshpurwar12@gmail.com>
2023-02-05 21:36:26 +0530

    fixing

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageNotFound() {
   return (
     <div>
-       <h2>There's nothing here: 404!</h2>
+       <h1>Page Not Found: 404!</h1>
     </div>
   )
 }
